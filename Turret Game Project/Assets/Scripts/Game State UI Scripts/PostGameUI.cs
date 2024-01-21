16b2773ff3a8a14e2251c62596b34a4be52d1da9
SiamNgu <81570033+SiamNgu@users.noreply.github.com>
2024-01-21 15:48:22 +0700

    Fix game over title not updating

--- Turret Game Project/Assets/Scripts/Game State UI Scripts/PostGameUI.cs
+++ Turret Game Project/Assets/Scripts/Game State UI Scripts/PostGameUI.cs
@@ -1,5 +1,9 @@
+using UnityEngine;
+using TMPro;
+
 public class PostGameUI : GameStateUIScript
 {
+    [SerializeField] private TMP_Text title;
     protected override GameManager.GameStateEnum gameState { get; set; } = GameManager.GameStateEnum.PostGame;
     protected override void Awake()
     {
@@ -7,6 +11,12 @@ public class PostGameUI : GameStateUIScript
     }
     protected override void SubscribeEvent()
     {
-        GameManager.Instance.EnterPostGameEvent += (string winner) => transform.GetChild(0).gameObject.SetActive(true);
+        GameManager.Instance.EnterPostGameEvent += OnEndGame;
+    }
+
+    private void OnEndGame(string winner)
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        title.text = winner + " has won the game!";
     }
 }
