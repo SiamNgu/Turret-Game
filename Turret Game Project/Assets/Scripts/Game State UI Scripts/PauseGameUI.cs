684600bf7bd217bab740a14216e14eda8e2fe31b
3rr0rD1v1d3d8yZ3r0 <qm.angelvin@gmail.com>
2024-02-10 16:53:38 +0700

    Pause Button Implemented UI
    If the Unity platform build is IOS or Android, it will enable the pause button at the top right of the screen, you can click it to pause the game.

--- Turret Game Project/Assets/Scripts/Game State UI Scripts/PauseGameUI.cs
+++ Turret Game Project/Assets/Scripts/Game State UI Scripts/PauseGameUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseGameUI : GameStateUIScript
 {
@@ -8,10 +9,20 @@ public class PauseGameUI : GameStateUIScript
     protected override void Awake()
     {
         base.Awake();
+        #if UNITY_ANDROID || UNITY_IOS
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(() => GameManager.Instance.TriggerSwitchState(GameManager.GameStateEnum.Paused));
+        #else
+            transform.GetChild(1).gameObject.SetActive(false);
+        #endif
     }
     protected override void SubscribeEvent()
     {
         GameManager.Instance.EnterPausedEvent += () => transform.GetChild(0).gameObject.SetActive(true);
         GameManager.Instance.EnterInGameEvent += () => transform.GetChild(0).gameObject.SetActive(false);
+        #if UNITY_ANDROID || UNITY_IOS
+                GameManager.Instance.EnterPausedEvent += () => transform.GetChild(1).gameObject.SetActive(false);
+                GameManager.Instance.EnterInGameEvent += () => transform.GetChild(1).gameObject.SetActive(true);
+        #endif
     }
 }
