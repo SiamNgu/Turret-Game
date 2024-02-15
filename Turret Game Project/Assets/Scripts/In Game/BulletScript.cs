1ef12bc01df4573d98aa4869ef54fd3a9ce66e15
SiamNgu <81570033+SiamNgu@users.noreply.github.com>
2024-02-15 13:37:42 +0700

    Revert "Merge branch 'animations'"
    This reverts commit 53afff1c5526d9d8575da122ae1e99222dc9338c, reversing
    changes made to 9f545dd2e6434b4cd7687ff18dea73b55f2ad548.

--- Turret Game Project/Assets/Scripts/In Game/BulletScript.cs
+++ Turret Game Project/Assets/Scripts/In Game/BulletScript.cs
@@ -1,32 +1,20 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 public class BulletScript : MonoBehaviour
 {
-    private Animator animator;
     [HideInInspector] public GunProfileScriptableObject profile;
 
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-    }
-
-    public void Move()
+    private void Update()
     {
         if (GameManager.Instance.gameState == GameManager.GameStateEnum.Paused) return;
         transform.Translate(Vector3.up * Time.deltaTime * profile.speed, Space.Self);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        animator.SetTrigger("Hit");
         if (other.tag == "Bullet")
-            other.GetComponent<Animator>().SetTrigger("Hit"); 
+            Destroy(other.gameObject);
         else if (other.tag == "Player")
             other.GetComponent<PlayerBase>().Health -= profile.damage;
-    }
-
-    public void OnExitStateMachine()
-    {
         Destroy(gameObject);
     }
 
